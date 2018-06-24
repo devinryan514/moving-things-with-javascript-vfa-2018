48b84548d69399d7791703de456e355658ff574a
Devin Ryan <devin.ryan514@gmail.com>
2018-06-24 04:35:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const app = "I don't do much."
 
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
 
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
